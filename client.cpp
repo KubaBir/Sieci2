883e56057e777bc479c46d0ed76ac51636e317d1
Jakub Birecki <kubabir@outlook.com>
2024-01-07 18:22:58 +0100

    added multi-client support and basic auth

--- client.cpp
+++ client.cpp
@@ -2,79 +2,130 @@
 #include <fcntl.h>  // for open
 #include <netinet/in.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+// #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>  // for close
 
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <string>
 
-using namespace std;
+int clientSocket;
+bool con_closed = false;
+pthread_t thread_id;  // Listener thread
+
+void sigintHandler(int signum) {
+    std::cout << "\nCTRL-C detected. Closing socket and exiting...\n";
+    if (close(clientSocket) != 0) std::cout << "Couldn't close the client socket\n";
+    exit(signum);
+}
+
+void setupSignalHandler() {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = sigintHandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+}
 
 void *socketThread(void *arg) {
     int newSocket = *((int *)arg);
 
     // Listen for a command
     char command[256];
-    recv(newSocket, command, sizeof command, 0);
+    while (1) {
+        if (recv(newSocket, command, sizeof(command), 0) == 0) {
+            std::cout << "Connection closed.\n"
+                      << "Press enter to quit\n";
+            con_closed = true;
+            break;
+        };
 
-    printf("Executing: %s\n", command);
-    if (system(command) != 0) cout << "Command execution failed!\n";
+        if (command[0] == '~') {
+            // Handle message (messages start with '~')
+            std::cout << command << "\n";
+        } else {
+            // Handle command
+            if (std::strlen(command) > 0) printf("Executing: %s\n", command);
+            if (system(command) != 0) std::cout << "Command execution failed!\n";
+        }
+        memset(&command, 0, sizeof(command));
+    }
 
+    close(newSocket);
     pthread_exit(NULL);
 }
 
-int main() {
-    char message[256];
+void registerClient(int socket, std::string name) {
+    std::string message = "REGISTER " + name;
+    if (send(socket, message.c_str(), message.length(), 0) < 0) {
+        printf("Send failed\n");
+    }
+}
 
-    char buffer[256];
-    int clientSocket;
+int main(int argc, char *argv[]) {
+    setupSignalHandler();
+    std::string message;
     sockaddr_in serverAddr;
-    socklen_t addr_size;
 
     // Create the socket.
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     // Configure settings of the server address
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(1100);
+    serverAddr.sin_port = htons(55554);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // Connect the socket to the server using the address
+    // Connect the socket to the server
     if (connect(clientSocket, (sockaddr *)&serverAddr, sizeof(serverAddr))) {
-        cout << "Failed to connect\n";
-        return 0;
+        std::cout << "Failed to connect " << errno << "\n";
+        return 1;
     }
 
-    // Create listener thread
-    pthread_t thread_id;
-    if (pthread_create(&thread_id, NULL, socketThread, &clientSocket) != 0)
-        printf("Failed to create thread\n");
-    // pthread_detach(thread_id);
+    // Register the client
+    if (argc < 2) {
+        std::cout << "Coudn't find the client name.\nUsage: ./client <name>\n";
+        return -1;
+    }
+    registerClient(clientSocket, argv[1]);
 
-    // Send command
-    cout << "Type the command you want to send:\n";
-    cin >> message;
-    if (send(clientSocket, message, sizeof(message), 0) < 0) {
-        printf("Send failed\n");
+    // Create listener thread
+    if (pthread_create(&thread_id, NULL, socketThread, &clientSocket) != 0) {
+        printf("Failed to create thread\n");
+        return 1;
+    }
+
+    std::cout << "Send 'q' to exit\n";
+    std::cout << "Type the command you want to send:\n";
+
+    while (1) {
+        // Send command
+        std::getline(std::cin, message);
+        if (!message.length()) continue;
+
+        // message - <client_name> <command>
+        message = "COMMAND " + message;
+        if (send(clientSocket, message.c_str(), message.length(), 0) < 0) {
+            if (!con_closed)
+                printf("Send failed\n");
+            return 1;
+        }
+
+        // Exit on 'q'
+        if (message == "q") {
+            std::cout << "Exiting...\n";
+            // The socket will be closed once the server closes its socket
+            break;
+        }
     }
 
     // Await thread execution to display the command effects
     pthread_join(thread_id, NULL);
-
-    // bzero(buffer, sizeof(buffer));
-    // // Read the message from the server into the buffer
-    // if (recv(clientSocket, buffer, sizeof(buffer), 0) < 0) {
-    //     printf("Receive failed\n");
-    // }
-    // // Print the received message
-    // printf("Data received: %s\n", buffer);
-
-    // memset(&message, 0, sizeof(message));
-
-    // close(clientSocket);
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -2,42 +2,197 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
 // gcc program.c -lpthread -Wall -o outfile Remember to link pthread bib
 
+const int one = 1;
+int serverSocket, newSocket;
 char client_message[256];
 char buffer[256];
 
+// Clients - [<name>, <socket>, <...permissions>]
+std::vector<std::tuple<std::string, int, std::vector<std::string>>> clients;
+
+void sigintHandler(int signum) {
+    std::cout << "\nCTRL-C detected. Closing socket and exiting...\n";
+    if (close(serverSocket) != 0) std::cout << "Couldn't close the server socket\n";
+    exit(signum);
+}
+
+void setupSignalHandler() {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = sigintHandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+}
+
+std::tuple<std::string, int, std::vector<std::string>> getUserBySocket(int socket) {
+    std::tuple<std::string, int, std::vector<std::string>> user;
+    bool found = false;
+    for (const auto &client : clients) {
+        if (std::get<1>(client) == socket) {
+            user = client;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        std::cout << "Could not authenticate the user.\n";
+        return std::make_tuple("", 0, std::vector<std::string>());
+    }
+    return user;
+}
+
+std::tuple<std::string, int, std::vector<std::string>> getUserByName(std::string name) {
+    std::tuple<std::string, int, std::vector<std::string>> user;
+    bool found = false;
+    for (const auto &client : clients) {
+        if (std::get<0>(client) == name) {
+            user = client;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        std::cout << "Could not authenticate the user.\n";
+        return std::make_tuple("", 0, std::vector<std::string>());
+    }
+    return user;
+}
+
+bool checkPermissions(std::string sender, std::string receiver) {
+    auto user = getUserByName(sender);
+
+    bool hasPermission = false;
+    auto permissions = std::get<2>(user);
+    for (const auto &permission : permissions) {
+        if (permission == receiver) {
+            hasPermission = true;
+            break;
+        }
+    }
+
+    return hasPermission;
+}
+
+void handleRegister(std::string name, int socket) {
+    // Add client to the registry
+    clients.push_back(std::make_tuple(name, socket, std::vector<std::string>{name, "user2"}));
+
+    std::cout << "Registered the client: " << name << "\n";
+
+    // DEBUG
+    // display clients
+
+    std::cout << "Current client list:\n";
+    for (const auto &client : clients) {
+        std::cout << "Name: " << std::get<0>(client)
+                  << "\n Socket: " << std::get<1>(client)
+                  << "\n Permissions: ";
+
+        for (const auto &str : std::get<2>(client)) {
+            std::cout << str << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void handleCommand(std::string command, std::string target_name, int socket) {
+    // Get user
+    auto user = getUserBySocket(socket);
+    auto target = getUserByName(target_name);
+
+    std::cout << "Parsing command from " << std::get<0>(user) << "\n";
+
+    // Check permissions
+    bool hasPermission = checkPermissions(std::get<0>(user), target_name);
+    if (!hasPermission) {
+        std::string res = "~You don't have sufficient permissions.";
+        send(socket, res.c_str(), res.length(), 0);
+        return;
+    }
+    // Send command to the target user
+    send(std::get<1>(target), command.c_str(), command.length(), 0);
+}
+
+void parseRequest(char *request, int socket) {
+    // Convert request to params
+    std::string action, param1, param2;
+    std::istringstream iss(request);
+    iss >> action >> param1 >> param2;
+
+    if (action == "REGISTER")
+        handleRegister(param1, socket);
+    if (action == "COMMAND")
+        handleCommand(param1, param2, socket);
+
+    // std::cout << "action: " << action << "\n";
+    // std::cout << "param1: " << param1 << "\nparam2: " << param2 << "\n";
+}
+
 void *socketThread(void *arg) {
-    int newSocket = *((int *)arg);
-    int n;
-    n = recv(newSocket, client_message, sizeof client_message, 0);
-    printf("%s\n", client_message);
+    int clientSocket = *((int *)arg);
 
-    send(newSocket, client_message, sizeof client_message, 0);
-    memset(&client_message, 0, sizeof(client_message));
+    while (1) {
+        // Await commands
+        if (recv(clientSocket, client_message, sizeof(client_message), 0) == 0) {
+            // Client closed the connection
+            break;
+        };
+        std::cout << "message: " << client_message << "\n";
 
+        if (strcmp(client_message, "COMMAND q") == 0) break;
+
+        parseRequest(client_message, clientSocket);
+        memset(&client_message, 0, sizeof(client_message));
+
+        // printf("Received command: %s\n", client_message);
+
+        // // Send command to the receiver
+        // send(clientSocket, client_message, sizeof(client_message), 0);
+    }
+
+    std::cout << "Closing socket\n";
+    if (close(clientSocket) != 0) std::cout << "Coudn't close the socket\n";
     pthread_exit(NULL);
 }
 
 int main() {
-    int serverSocket, newSocket;
-    struct sockaddr_in serverAddr;
-    struct sockaddr_storage serverStorage;
-    socklen_t addr_size;
+    setupSignalHandler();
 
     // Create the socket.
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        perror("Error creating socket");
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(1100);
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(55554);
+    // serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     // Bind the address struct to the socket
-    bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cout << "Couldn't bind " << errno << "\n";
+        return 1;
+    };
+    std::cout << "Server running on port: " << ntohs(serverAddr.sin_port) << std::endl;
 
     if (listen(serverSocket, 10) == 0)
         printf("Listening\n");
@@ -48,9 +203,7 @@ int main() {
 
     while (1) {
         // Accept call creates a new socket for the incoming connection
-        addr_size = sizeof serverStorage;
-        newSocket = accept(serverSocket, (struct sockaddr *)&serverStorage,
-                           &addr_size);
+        newSocket = accept(serverSocket, nullptr, nullptr);
 
         if (pthread_create(&thread_id, NULL, socketThread, &newSocket) != 0)
             printf("Failed to create thread\n");
